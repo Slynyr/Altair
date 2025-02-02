b62b35f31b73c9626f932b1222e221f85d620943
Slynyr <99285452+Slynyr@users.noreply.github.com>
2025-02-01 22:33:56 -0500

    Implemented PyQt

--- altair_app/src/main.py
+++ altair_app/src/main.py
@@ -1,7 +1,11 @@
 from backend.gemini_parser import Geminiparser
 from dotenv import load_dotenv
 from backend.constants import Constants
+from backend.pdf_parser import PDFParser
 import base64
+import sys
+from PyQt6.QtWidgets import QApplication
+from backend.threejs_app import ThreeJsApp
 
 
 def temp_read(file_path):
@@ -13,12 +17,17 @@ def temp_read(file_path):
 
 load_dotenv()
 
-def __main__():
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ThreeJsApp()
+    window.show()
+    sys.exit(app.exec())
+
     m_geminiParser = Geminiparser()
+    m_PDFParser = PDFParser()
 
-    temp_data = temp_read("page-5.pdf")
+    temp_data = m_PDFParser.get_page_data("full-test.pdf")
 
-    print(m_geminiParser.remix_questions(Constants.Gemini.TEMP_QUESTION))
-
-__main__()
+    extracted_question = m_geminiParser.extract_questions(temp_data[4])[0]
+    print(m_geminiParser.remix_questions(extracted_question))
     
