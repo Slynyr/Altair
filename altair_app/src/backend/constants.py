a4121c845e6ef7ad8932a870d0971ebb2f2bced0
johnwarp <samuelzhao0121@gmail.com>
2025-02-01 20:07:55 -0500

    Constants file

---
+++ altair_app/src/backend/constants.py
@@ -0,0 +1,3 @@
+class Constants:
+    class Gemini:
+        EXTRACT_PROMPT = "Give me all the question in this paper in latex form. Please spend extra time ensuring that any existing equations are correctly identified. Please include the question itself and any formulas if any. Do not provide any additional text in your response that is not in the paper"
