96ce6ce4a9c84b2e5612f61a261b92644a8f4a5d
mli57 <maxwell.l4991@gmail.com>
2025-02-01 23:11:49 -0500

    Update constants.py

--- altair_app/src/backend/constants.py
+++ altair_app/src/backend/constants.py
@@ -1,5 +1,9 @@
 class Constants:
     class Gemini:
+        #Generate a Latex version of every question from the input file. 
+        #For each question, include the complete text and any formulas or equations exactly as they appear. 
+        #Make sure that all equations are correctly identified and rendered in LaTeX. 
+        #Do not include any additional commentary or text beyond what is present in the paper.
         EXTRACT_PROMPT = "Give me all the question in this paper in latex form. Please spend extra time ensuring that any existing equations are correctly identified. Please include the question itself and any formulas if any. Do not provide any additional text in your response that is not in the paper"
         REMIX_PROMPT = "Create a very similar question to the following. If equations are present, try to keep close to the original in order to not change the question too much. Provide the new question in full latex format. Please do not add any additional text to your output."
         TEMP_QUESTION = """20. Find the radius of convergence of the following power series.
