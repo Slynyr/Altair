4721e90c9e809c6aa9759fc4bce28ba2ba4dc0e3
mli57 <maxwell.l4991@gmail.com>
2025-02-01 22:58:58 -0500

    Update pdf_parser.py

--- altair_app/src/backend/pdf_parser.py
+++ altair_app/src/backend/pdf_parser.py
@@ -3,46 +3,31 @@ import PyPDF2
 import io
 import base64
 
+#create class for parsing pdf
 class PDFParser:
     def __init__(self):
         pass
     
     
     def get_page_data(self, file_path):
-        with open(file_path, 'rb') as pdf_file:
+        with open(file_path, 'rb') as pdf_file: #opens pdf file, creating a reader and writer
             reader = PyPDF2.PdfReader(pdf_file)
             writer = PyPDF2.PdfWriter()
 
-            if len(reader.pages) == 0:
+            if len(reader.pages) == 0: #returns if there is no pdf
                 print("PDF is empty")
                 return []
             
-            output_data = []
+            output_data = [] #empty list to store base64 based string which represents each page
             
-            for i in range(0, len(reader.pages)):
+            for i in range(0, len(reader.pages)): #creates a new pdf write for each page and adds page to the writer
                 writer = PyPDF2.PdfWriter()
                 writer.add_page(reader.pages[i])
 
-                output_stream = io.BytesIO()
-                writer.write(output_stream)
-                output_stream.seek(0)
+                output_stream = io.BytesIO() #stores the pdf
+                writer.write(output_stream) 
+                output_stream.seek(0) #stream position is reset
 
                 base64_encoded = base64.b64encode(output_stream.getvalue()).decode('utf-8')
                 output_data.append(base64_encoded)
         return output_data
-
-    def save_first_page(self, input_pdf):
-        with open(input_pdf, 'rb') as pdf_file:
-            reader = PyPDF2.PdfReader(pdf_file)
-            writer = PyPDF2.PdfWriter()
-
-            if len(reader.pages) == 0:
-                print("PDF is empty")
-                return
-
-            first_page = reader.pages[0]
-            writer.add_page(first_page)
-
-            with open("out.pdf", 'wb') as output_file:
-                writer.write(output_file)
-        return first_page
